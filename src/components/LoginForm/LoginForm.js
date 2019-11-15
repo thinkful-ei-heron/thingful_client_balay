4d06ddc9211705d06bca897e4c92dc85620d7713
balayaydemir <balayaydemir@gmail.com>
2019-11-15 12:08:53 -0800

    expiry implemented

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import { Button, Input } from '../Utils/Utils'
-import TokenService from '../../services/token-service'
 import AuthApiService from '../../services/auth-api-service'
 
 export default class LoginForm extends Component {
@@ -9,19 +8,6 @@ export default class LoginForm extends Component {
   }
 
   state = { error: null }
-
-  handleSubmitBasicAuth = ev => {
-    ev.preventDefault()
-    const { user_name, password } = ev.target
-
-    TokenService.saveAuthToken(
-      TokenService.makeBasicAuthToken(user_name.value, password.value)
-    )
-
-    user_name.value = ''
-    password.value = ''
-    this.props.onLoginSuccess()
-  }
 
   handleSubmitJwtAuth = ev => {
     ev.preventDefault();
@@ -35,7 +21,6 @@ export default class LoginForm extends Component {
       .then(res => {
         user_name.value = ''
         password.value = ''
-        TokenService.saveAuthToken(res.authToken)
         this.props.onLoginSuccess()
       })
       .catch(res => {
